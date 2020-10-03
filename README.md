c83e1a31545622dbb8a17372ac6a800676fc97f8
lianqiann <al2374@cornell.edu>
2020-10-03 13:16:37 -0400

    update db content in maindb

--- README.md
+++ README.md
@@ -14,3 +14,23 @@ Develop a full-functioning art search engine.
 ### Relevant Doc
 [Google Doc](https://docs.google.com/document/d/1wCjr7nEeb-J4IZ7t8HJxeMzG3G4R0kNWznpK6ybn7o0/edit)
 
+### gcp MySQL 
+
+instance_id: artimages
+<br>
+root password: ArtSearchAm295
+
+Current method: 
+[Connect Cloud SQL to Python - pymysql](https://cloud.google.com/sql/docs/mysql/connect-external-app#pymysql-tcp)
+- Install Proxy on Mac: <br>
+`curl -o cloud_sql_proxy https://dl.google.com/cloudsql/cloud_sql_proxy.darwin.amd64`<br>
+`chmod +x cloud_sql_proxy`<br>
+- Start Proxy in cmd:`./cloud_sql_proxy -instances=ac295-data-science-289013:us-east1:artimages=tcp:3306`<br>
+Note: `ac295-data-science-289013:us-east1:artimages` is Selina's sql instance <br>
+- In python: `python sql_test.py`
+
+
+Previous method (ignore):
+[instruction](https://cloud.google.com/sql/docs/sqlserver/quickstart)
+Exist error from SSMS.
+
--- arts_database/maindb.py
+++ arts_database/maindb.py
@@ -6,38 +6,6 @@ import numpy as np
 from sklearn.decomposition import PCA
 META_FOLDER = 'arts_database/static/database/gap_images'
 
-############################## filename_frontend #######################################
-from sqlalchemy import create_engine
-engine = create_engine('mysql+pymysql://root:ArtSearchAm295@127.0.0.1/artsearch')
-app = Flask(__name__)
-
-      
-@app.route('/', methods=['POST', 'GET'])
-def index():
-    if request.method=='POST':
-        # receive input from name content in html
-        Title = request.get_json()['artName'] 
-        print("Get title: ", Title)
-        try:
-            
-            sql = "SELECT `Artist`, `Title`, `image_src` FROM `artsimg` WHERE `Title`=%s"
-            #engine.execute(sql, ('Bambocciata (Childishness)',))
-            art_obj = engine.execute(sql, (Title,)).fetchall()[0]
-            
-            return '{};{};{}'.format(art_obj[0], art_obj[1], art_obj[2]) 
-        except:
-            art_obj = {"Title":"No Input",
-                   "image_src":"None", 
-                   "Artist":"None"}
-            return art_obj
-            
-    else:
-        return "maindb.py - This is get method - try using post -- "
-
-############################## filename_frontend #######################################
-
-
-
 def cos_similarity(A,B):
     # A and B are both (32,) numpy array
     res = np.dot(A,B)/(np.linalg.norm(A)*np.linalg.norm(B))
@@ -59,7 +27,6 @@ def read_image():
     return img
 
     
-@app.route("/",methods=['POST','GET'])
 def search_image():
     img_obj = read_image()
     filename = ''
@@ -78,10 +45,54 @@ def search_image():
         url = ''
     else:
         # TODO: read metadata from db
+        # Title = filename
+        # sql = "SELECT `image_src` FROM `artsimg` WHERE `Title`=%s"
+        # url = engine.execute(sql, (Title,)).fetchall()[0]
+
         meta = pd.read_csv(os.path.join(META_FOLDER, 'metadata.csv'))
         url = meta[meta['file_id'] == filename]['image_src'].values[0]
 
     return url
+
+def match_image():
+    Title = request.get_json()['artName'] 
+    print("Get title: ", Title)
+    try:
+        sql = "SELECT `Artist`, `Title`, `image_src` FROM `artsimg` WHERE `Title`=%s"
+        #engine.execute(sql, ('Bambocciata (Childishness)',))
+        art_obj = engine.execute(sql, (Title,)).fetchall()[0]
+            
+        return '{};{};{}'.format(art_obj[0], art_obj[1], art_obj[2]) 
+    except:
+        art_obj = {"Title":"No Input",
+                "image_src":"None", 
+                "Artist":"None"}
+        return art_obj
+
+
+############################## filename_frontend #######################################
+from sqlalchemy import create_engine
+engine = create_engine('mysql+pymysql://root:ArtSearchAm295@127.0.0.1/artsearch')
+app = Flask(__name__)
+
+      
+@app.route('/', methods=['POST', 'GET'])
+def index():
+    if request.method=='POST':
+        # receive input from name content in html
+        if request.get_json():
+            art_obj = match_image()
+            return art_obj
+        elif request.files:
+            url = search_image()
+            return url
+            
+    else:
+        return "maindb.py - This is get method - try using post -- "
+
+############################## filename_frontend #######################################
+
+
 
 
 
